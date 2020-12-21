4c8b461b8b078d354540da52ce37bb01e5ea2bdf
Groucho <abc@def.com>
2020-12-21 22:48:23 +0100

    Added a new comment on feature_001 branch

--- hello.go
+++ hello.go
@@ -4,5 +4,7 @@ import "fmt"
 
 // Dummy comment
 func main() {
-    fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
 }
+
+//OK, now I created a new comment on feature_001 branch - time to commit!
